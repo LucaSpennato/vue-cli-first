ce9ba6e8565bc168a7aa757c5e083b25b3e19746
LucaSpennato <luca.spennato@gmail.com>
2022-07-14 17:20:22 +0200

    milestone 1

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+        {{ title }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleComp',
+    data: function(){
+         return {
+            title: 'La mia prima app con Vue CLI!',
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
